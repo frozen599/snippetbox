05d036e55cd64b1b9d059e8e412a09739880a88e
tuong <lovesunshine269@gmail.com>
2022-02-19 21:38:09 +0700

    fix severe typos

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -63,7 +63,7 @@ func (f *Form) MinLength(field string, d int) {
 		return
 	}
 	if utf8.RuneCountInString(value) < d {
-		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters", d))
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
 	}
 }
 
